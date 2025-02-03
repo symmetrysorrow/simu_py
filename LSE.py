1b2fdc77f252c7d9047519417dd7ec29de185b19
symmetrysorrow <92022965+symmetrysorrow@users.noreply.github.com>
2025-02-03 15:23:10 +0900

    0.14

--- LSE.py
+++ LSE.py
@@ -5,7 +5,8 @@ from scipy.optimize import minimize
 
 output = "output"
 
-
+eta = 101
+amp = 10
 
 def MakePulse(para):   
     makepulse_dll = ctypes.CDLL('./MakePulse.dll')
@@ -29,17 +30,27 @@ def LoadExprimetalData(para):
     data = np.loadtxt(f"{output}/{para["E"]}keV_{para["position"][0]}/pulse/CH0/CH0_1.dat")
     return pulses
 
+
+
+with open(f"{output}/input.json", "r") as f:
+    para = json.load(f)
+
+ExperimentalData=[]
+
+cnt=1
+for i in para["position"]:  
+    #data  = np.loadtxt(f"output/averagepulses/average_pulse_{cnt}.txt")/amp*eta
+    data =  np.loadtxt(f"E:/tagawa/exp_py/average_pulse-block_{cnt}.dat")/amp*eta
+    data = data[5000:10000]
+    ExperimentalData.append(data)
+    cnt+=1
+
 initial_params = [7.9e-10,
                 7.9e-12,
                 1.5e-07,
                 8.2e-09,
                 1.68e-08
                 ]
-
-with open(f"{output}/input.json", "r") as f:
-    para = json.load(f)
-
-ExperimentalData=[]
 
 def err_func(params):
     para["C_abs"] = params[0]
@@ -53,6 +64,8 @@ def err_func(params):
     err = 0
     for i in range(len(pulses)):
         err += np.sum((pulses[i] - ExperimentalData[i])**2)
+
+    return err
 
 result = minimize(err_func, initial_params, method="Nelder-Mead", 
                   options={'maxiter': 500, 'disp': True, 'xatol': 1e-6, 'fatol': 1e-6})
